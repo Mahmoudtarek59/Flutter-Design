284e12175ed10131d04a7bf62a07ea45c9d010a3
Mahmoud tarek <tmahmoud974@gmail.com>
2020-02-04 21:01:21 +0200

    popAndPush

---
+++ Navigation/3-popAndPush/lib/Screen/SecondScreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+class SecondScreen extends StatefulWidget{
+  @override
+  _SecondScreenState createState()=> new _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen>{
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("SecondScreen"),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(30.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text("SecondScreen"),
+              new RaisedButton(onPressed: ()=>Navigator.of(context).pushNamedAndRemoveUntil("/Third",(Route<dynamic> route)=>false),child: new Text("Next"),),
+              new RaisedButton(onPressed: (){
+                if(Navigator.of(context).canPop()){
+                  Navigator.of(context).pop();
+                }else{
+                  Navigator.of(context).pushNamedAndRemoveUntil("/Home",(Route<dynamic> route)=>false);
+                }
+              },child: new Text("Back"),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ Navigation/3-popAndPush/lib/Screen/ThirdScreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+
+class ThirdScreen extends StatefulWidget{
+  @override
+  _ThirdScreenState createState()=> new _ThirdScreenState();
+}
+
+class _ThirdScreenState extends State<ThirdScreen>{
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("ThirdScreen"),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(30.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text("ThirdScreen"),
+              new RaisedButton(onPressed: (){
+                if(Navigator.of(context).canPop()){
+                  Navigator.of(context).pop();
+                }else{
+                  Navigator.of(context).pushNamedAndRemoveUntil("/Second", (Route<dynamic> route)=>false);
+                }
+              },child: new Text("Back"),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
